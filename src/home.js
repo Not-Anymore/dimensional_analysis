b407c9c6282c78f1d767bd86499210eb5b1e4067
Didier MUNEZERO <didiermunezer38@gmail.com>
2020-12-31 10:03:25 +0200

    fetching dummy data from json placeholder

--- src/home.js
+++ src/home.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Select from "react-select";
+import axios from "axios";
 
-const quantities = [
-  { label: "Density", value: "Density" },
-  { label: "Volume", value: "Volume" },
-  { label: "Force", value: "Force" },
-  { label: "Work", value: "Work" },
-  { label: "Energy", value: "Energy" },
-  { label: "Efficiency", value: "Efficiency" },
-];
+// const quantities = [
+//   { label: "Density", value: "Density" },
+//   { label: "Volume", value: "Volume" },
+//   { label: "Force", value: "Force" },
+//   { label: "Work", value: "Work" },
+//   { label: "Energy", value: "Energy" },
+//   { label: "Efficiency", value: "Efficiency" },
+// ];
 
 const Home = () => {
+  const [quantity, setQuantity] = useState([]);
+  const [analysis, SetAnalysis] = useState([]);
+  const getQuantities = async () => {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts`
+    );
+    response.data.forEach((element) => {
+      element.label = element.id;
+    });
+    console.log(response.data[9]);
+    setQuantity(response.data);
+  };
+  useEffect(() => {
+    getQuantities(quantity);
+  }, []);
   return (
     <div>
       <div className="header">
@@ -18,7 +34,11 @@ const Home = () => {
       </div>
 
       <div className="row">
-        <Select options={quantities} className="select" />
+        <Select
+          options={quantity}
+          onChange={(opt) => console.log(opt)}
+          className="select"
+        />
       </div>
       <div className="solution">
         <p>---For Your analysis---</p>
