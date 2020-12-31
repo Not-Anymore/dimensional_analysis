619ad054442b3ffe265d904e49078a6eb54dd637
Didier MUNEZERO <didiermunezer38@gmail.com>
2020-12-31 09:16:28 +0200

    re look at the file

--- src/home.js
+++ src/home.js
@@ -3,28 +3,16 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h2>CSS Template using Flexbox</h2>
-      <p>
-        In this example, we have created a header, three unequal columns and a
-        footer. On smaller screens, the columns will stack on top of each other.
-        Resize the browser window to see the responsive effect.
-      </p>
-      <p>
-        <strong>Note:</strong> Flexbox is not supported in Internet Explorer 10
-        and earlier versions.
-      </p>
-
       <div className="header">
-        <h2>Header</h2>
+        <h2>Dimensional analysis Calculator</h2>
       </div>
 
       <div className="row">
-        <div className="column side">Column</div>
-        <div className="column middle">Column</div>
+        <input />
       </div>
 
       <div className="footer">
-        <p>Footer</p>
+        <p>This will be footer</p>
       </div>
     </div>
   );
